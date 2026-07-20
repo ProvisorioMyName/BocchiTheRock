1f52f484c52c4377f554eadb8e1f1f5d4727554e
ProvisorioMyName <provisoriomyname@gmail.com>
2026-07-19 22:17:24 -0300

    2/3 da parte das informações

--- assets/banda.js
+++ assets/banda.js
@@ -8,6 +8,24 @@ const personagens = {
     instrumento1: '../imagens/icones/graves.png',
     instrumento2: '',
     sobre: 'Hitori Gotoh (Bocchi) é uma estudante extremamente tímida e com forte ansiedade social, mas que esconde um talento excepcional na guitarra. Após passar anos praticando sozinha trancada em seu armário e postando vídeos na internet como "guitarhero", ela foi convidada a se juntar à Kessoku Band, iniciando sua jornada para superar seus medos e se conectar com os outros através da música.',
+    icones:[
+    '../imagens/hitori-icons/calendario-rosa.png',
+    '../imagens/hitori-icons/altura-rosa.png',
+    '../imagens/hitori-icons/graves-rosa.png',
+    '../imagens/hitori-icons/comida-rosa.png',
+    '../imagens/hitori-icons/gota-rosa.png',
+    '../imagens/hitori-icons/diplomado-rosa.png',
+    ],
+    informações:[
+        '21 de fevereiro',
+        '156cm',
+        'Guitarra',
+        'Yakisoba Pan',
+        'O',
+        '1° Ano do Ensino Médio',
+    ],
+
+
   },
   nijika: {
     cor: "#f7c600",
@@ -17,7 +35,23 @@ const personagens = {
     posicao: 'Baterista',
     instrumento1: '../imagens/icones/bateria.png',
     instrumento2: '',
-    sobre: 'Nijika Ijichi é a enérgica, organizada e dedicada baterista da Kessoku Band. Sendo a fundadora do grupo ao lado de Ryo, ela assume o papel de líder e é a força acolhedora que mantém todas unidas. Seu grande sonho é transformar a Kessoku Band em um enorme sucesso, em parte para homenagear sua irmã mais velha e apoiar a casa de shows STARRY.'
+    sobre: 'Nijika Ijichi é a enérgica, organizada e dedicada baterista da Kessoku Band. Sendo a fundadora do grupo ao lado de Ryo, ela assume o papel de líder e é a força acolhedora que mantém todas unidas. Seu grande sonho é transformar a Kessoku Band em um enorme sucesso, em parte para homenagear sua irmã mais velha e apoiar a casa de shows STARRY.',
+    icones:[
+    '../imagens/nijika-icons/calendario-amarelo.png',
+    '../imagens/nijika-icons/altura-amarelo.png',
+    '../imagens/nijika-icons/bateria.png',
+    '../imagens/nijika-icons/comida-amarelo.png',
+    '../imagens/nijika-icons/gota-amarelo.png',
+    '../imagens/nijika-icons/diplomado-amarelo.png',
+    ],
+        informações: [
+        '29 de maio',
+        '154cm',
+        'Bateria',
+        'Doces ou bubble Tea',
+        'A',
+        '1° Ano do Ensino Médio'
+    ],    
   },
   ryo: {
     cor: "#1e4bff",
@@ -28,9 +62,26 @@ const personagens = {
     instrumento1: '../imagens/icones/bass.png',
     instrumento2: '',
     sobre: 'Ryo Yamada é a excêntrica, calma e reservada baixista da Kessoku Band, sendo também a mente responsável por compor as melodias da banda. Ela tem uma personalidade única, valoriza profundamente sua individualidade e prefere passar o tempo sozinha, mantendo um estilo de vida peculiar que muitas vezes intriga suas amigas.',
+        icones:[
+    '../imagens/yamada-icons/calendario-azul.png',
+    '../imagens/yamada-icons/altura-azul.png',
+    '../imagens/yamada-icons/bass.png',
+    '../imagens/yamada-icons/comida-azul.png',
+    '../imagens/yamada-icons/gota-azul.png',
+    '../imagens/yamada-icons/diplomado-azul.png',
+    ],
+    informações:[
+        '18 de setembro',
+        '163cm',
+        'Contrabaixo',
+        'Qualquer coisa barata',
+        'A',
+        '1° Ano do Ensino Médio',
+    ],
+   },
 
-  },
-  kita: {
+
+  kita:{
     cor: "#ff174d",
     Tsombra: '0px 0px 5px #ff174d8c',
     nome: "Ikuyo Kita",
@@ -38,9 +89,25 @@ const personagens = {
     posicao: 'Vocalista/Guitarrista',
     instrumento1: '../imagens/icones/guitarravermelha.png',
     instrumento2: '../imagens/icones/microfone.png',
-    sobre: 'Ikuyo Kita é a vocalista e guitarrista secundária da Kessoku Band. Com uma personalidade radiante, otimista e extremamente sociável, ela é o completo oposto de Bocchi. Kita brilha tanto que sua energia positiva e presença nas redes sociais funcionam como o principal motor de divulgação e carisma da banda.'
-  },
-};
+    sobre: 'Ikuyo Kita é a vocalista e guitarrista secundária da Kessoku Band. Com uma personalidade radiante, otimista e extremamente sociável, ela é o completo oposto de Bocchi. Kita brilha tanto que sua energia positiva e presença nas redes sociais funcionam como o principal motor de divulgação e carisma da banda.',
+    icones:[
+    '../imagens/kita-icons/calendario-vermelho.png',
+    '../imagens/kita-icons/altura-vermelho.png',
+    '../imagens/kita-icons/guitarravermelha.png',
+    '../imagens/kita-icons/comida-vermelha.png',
+    '../imagens/kita-icons/gota-vermelha.png',
+    '../imagens/kita-icons/diplomado-vermelha.png',],
+    informações: [
+        '16 de outubro',
+        '158cm',
+        'Guitarra',
+        'Doces de café',
+        'A',
+        '1° Ano do Ensino Médio'
+        ],
+   },
+
+}
 
 
 
@@ -83,4 +150,20 @@ function atualizarPerfil(personagem) {
   }
 
   sobre.innerText = personagem.sobre
+}
+
+function atualizarInfo(personagem){
+    let border = document.getElementById('infos')
+    let Tinfo = document.querySelector('.subtitulo')
+    let imagensIcones = document.querySelectorAll('.info-img img')
+
+    border.style.borderColor = personagem.cor
+
+    Tinfo.style.color = personagem.cor
+    Tinfo.style.textShadow = personagem.Tsombra
+
+    for(let i = 0; i < imagensIcones.length; i++){
+        imagensIcones[i].src = personagem.icones[i]
+    }
+
 }

f2732abf8dcbab5dccdba4a268be77636d21e7d2
ProvisorioMyName <provisoriomyname@gmail.com>
2026-07-27 14:31:41 -0300

    Funcionalidade da linha do tempo

--- assets/sobre.js
+++ assets/sobre.js
@@ -26,7 +26,55 @@ function selectInfo(id){
     let conteudo = document.getElementById('conteudo')
     let caixa = document.querySelector('.info-box')
 
+
     titulo.innerText = info.titulo
     conteudo.innerText = info.conteudo
     caixa.style.display = 'block'
 }
+
+const linhas ={
+    linha1: {
+        numero: '1',
+        titulo: 'Uma vida isolada',
+        texto: 'Hitori passa seu tempo tocando guitarra e postando covers na internet.',
+        foto: '../imagens/anime/hitori.jpg'
+    },
+    linha2: {
+        numero: '2',
+        titulo: 'O encontro',
+        texto: 'Ela conhece Nijika, que a convida para entrar na Kessoku Band.',
+        foto: '../imagens/anime/nijikagoto.jpg'
+    },
+    linha3: {
+        numero: '3',
+        titulo: 'Primeiros passos',
+        texto: 'Hitori começa a ensaiar e enfrentar o desafio de tocar com outras pessoas.',
+        foto: '../imagens/anime/treinohitori.jpg'
+    },
+    linha4: {
+        numero: '4',
+        titulo: 'Novas Aamizades',
+        texto: 'Ela cria laços verdadeiros com a banda e descobre que não está sozinha.',
+        foto: '../imagens/anime/grupo.jpg'
+    },
+    linha5: {
+        numero: '5',
+        titulo: 'No palco',
+        texto: 'Bocchi supera seus medos e finalmente sente a alegria de tocar em um show.',
+        foto: '../imagens/anime/banda.jpg'
+    }
+}
+function selectTempo(id){
+    const tempo = linhas[id]
+
+    let num = document.getElementById('tempo-num')
+    let til = document.getElementById('tempo-ti')
+    let txt = document.getElementById('tempo-txt')
+    let img = document.querySelector('.tempo-img')
+
+    num.innerText = tempo.numero
+    til.innerText = tempo.titulo
+    txt.innerText = tempo.texto
+    img.style.backgroundImage = `url('${tempo.foto}')`
+}
+
